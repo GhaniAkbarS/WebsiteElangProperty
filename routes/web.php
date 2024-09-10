e8e580c3ce595c96c6446b0e3dc66e2ea3836a34
GhaniAkbarS <tigerfunk90@gmail.com>
2024-09-10 15:17:47 +0700

    perbaikan route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function ()  {
-    return view('beranda');
+   // return view('beranda');
 });
 
 Route::get('/dashboard', function () {
