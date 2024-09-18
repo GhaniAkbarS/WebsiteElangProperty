1d34944a2afed6611d638b3d67d417cbe2a9de61
GhaniAkbarS <tigerfunk90@gmail.com>
2024-09-18 22:08:13 +0700

    category form request

--- app/Http/Controllers/Backsites/Master/Category/CategoryController.php
+++ app/Http/Controllers/Backsites/Master/Category/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backsites\Master\Category;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest; // import Form Request
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -29,18 +30,14 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CategoryRequest $request)
     {
-
-        $request->validate([
-            'title' => 'required|string|max:255',
-            'content' => 'required|string',
-        ]);
-
+        // Simpan data ke database
         Category::create([
             'title' => $request->title,
+            'slug' => Str::slug($request->title), // slug otomatis dari judul
             'content' => $request->content,
-            'slug' => Str::slug($request->title),
+            'image' => $request->image ? $request->file('image')->store('categories') : null, // simpan gambar
         ]);
 
         return redirect()->route('category.index')->with('success', 'Category created successfully');
@@ -66,26 +63,20 @@ class CategoryController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public  function update(Request $request, $id)
+    public function update(CategoryRequest $request, Category $category)
     {
-        // Validasi input
-        $request->validate([
-            'title' => 'required|string|max:255',
-            'content' => 'required|string',
-        ]);
-
-        // Mencari data kategori berdasarkan ID dan memperbarui
-        $category = Category::findOrFail($id);
+        // Update data kategori
         $category->update([
             'title' => $request->title,
-            'slug' => Str::slug($request->title),
+            'slug' => Str::slug($request->title), // slug otomatis dari judul
             'content' => $request->content,
+            'image' => $request->image ? $request->file('image')->store('categories') : $category->image, // update gambar jika ada
         ]);
 
         return redirect()->route('category.index')->with('success', 'Category updated successfully');
     }
 
-    /**
+    /** 
      * Remove the specified resource from storage.
      */
     // Menghapus data kategori
