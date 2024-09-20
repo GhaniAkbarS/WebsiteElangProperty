c7fec8338e201efe7e7fb26c376270a360f45ba9
GhaniAkbarS <tigerfunk90@gmail.com>
2024-09-20 10:37:38 +0700

    home slider update

--- app/Http/Controllers/Frontsites/home/HomeController.php
+++ app/Http/Controllers/Frontsites/home/HomeController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers\Frontsites\home;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Slide; // Tambahkan model Slide untuk mengambil data dari tabel ep_slide
 
-// class ini yang ada pada route web.php yang berisi index di home frontsites pages
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return view('pages.frontsites.home.index');
+        // Mengambil data slide dari database, misalnya 3 slider terbaru
+        $slides = Slide::latest()->take(3)->get();
+
+        // Kirim data slides ke view
+        return view('pages.frontsites.home.index', compact('slides'));
     }
 }
--- resources/views/includes/backsites/meta.blade.php
+++ resources/views/includes/backsites/meta.blade.php
@@ -1,4 +1,4 @@
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
 <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-<title>ElangProperty.</title>
+<title>ElangProperty | Backsites </title>
